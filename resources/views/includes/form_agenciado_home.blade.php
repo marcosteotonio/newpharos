f8456ba8715ad42d7dbf3e0da3bd57cefbb51f36
Edson Rogerio Bedana <d.sis04@beneficiosocial.com.br>
2019-04-28 23:10:43 -0300

    register

--- resources/views/includes/form_agenciado_home.blade.php
+++ resources/views/includes/form_agenciado_home.blade.php
@@ -65,23 +65,59 @@
     </div>
 
     <div id="ag_form__resend" style="display: none;">
-        {!! Form::open(['method' => 'post', 'url' => '#', 'onsubmit' => 'return false;' ])!!}
+        <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
+            @csrf
+
+            <div class="form-group ">
+                <label for="name" style="font-size: 14px; font-weight: 200;">nome</label>
+
+                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+
+                @if ($errors->has('name'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('name') }}</strong>
+                    </span>
+                @endif
+            
+            </div>
+
+            <div class="form-group ">
+                <label for="email" style="font-size: 14px; font-weight: 200;">E-mail</label>
+
+                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+
+                @if ($errors->has('email'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
+            </div>
+
             <div class="form-group">
-                {!! Form::label('email', 'E-mail', ['style' => 'font-size: 14px; font-weight: 200;'])!!}
-                {!! Form::text('email', null, ['class' => 'form-control'])!!}
+                <label for="password" style="font-size: 14px; font-weight: 200;">senha</label>
+
+                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                @if ($errors->has('password'))
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
             </div>
+
             <div class="form-group">
-                {!! Form::label('password', 'Senha', ['style' => 'font-size: 14px; font-weight: 200;'])!!}
-                {!! Form::password('password', ['class' => 'form-control'])!!}
+                <label for="password-confirm" style="font-size: 14px; font-weight: 200;">Confirmar senha</label>
+                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
             </div>
-            <div class="form-group">
-                {!! Form::label('password_confirm', 'Confirmar Senha', ['style' => 'font-size: 14px; font-weight: 200;'])!!}
-                {!! Form::password('password', ['class' => 'form-control'])!!}
+
+            <div class="form-group row mb-0">
+                <div class="col-md-6 offset-md-4">
+                    <button type="submit" class="btn btn-primary">
+                        Cadastrar
+                    </button>
+                </div>
             </div>
-            <div class="text-center padding-bottom">
-            {!! Form::submit('CADASTRAR', ['class' => 'btn btn-access']) !!}
-            </div>
-        {!! Form::close()!!}
+        </form>
         <div class="text-center padding-bottom">
             <hr>
         </div>
