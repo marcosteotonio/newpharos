a6e24b227f771fc68cc75de7e68b133aa78eb6d0
Marcos Teotônio <d.sis06@beneficiosocial.com.br>
2019-05-10 10:17:02 -0300

    foi de novo

--- resources/views/emails/site/contato_email.blade.php
+++ resources/views/emails/site/contato_email.blade.php
@@ -1,5 +1,4 @@
-<p>Nome: {{dd($dados)}}</p>
-{{--<p>E-mail: {{$dados->email}}</p>--}}
-{{--<p>Telefone: {{$dados->phone}}</p>--}}
-{{--<p>Mensagem: {{$dados->message}}</p>--}}
-
+<p>Nome: {{$dados['name']}}</p>
+<p>E-mail: {{$dados['email']}}</p>
+<p>Telefone: {{$dados['phone']}}</p>
+<p>Mensagem: {{$dados['message']}}</p>
