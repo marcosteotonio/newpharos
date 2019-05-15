ecaa82c42b5a09dff881f00408d1e3c91b3cd9ac
Edson Rogerio Bedana <d.sis04@beneficiosocial.com.br>
2019-05-14 23:09:25 -0300

    migration timestamp error

---
+++ database/migrations/2019_05_15_014219_notice-videos-and-agenciado.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class NoticeVideosAndAgenciado extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notices', function (Blueprint $table) {
+            $table->string('video');
+            $table->jsonb('agenciado');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('notices', function (Blueprint $table) {
+            $table->dropColumn('video');
+            $table->dropColumn('agenciado');
+        });
+    }
+}
